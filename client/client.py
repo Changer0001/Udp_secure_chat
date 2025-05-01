8a890187f8f619e64a53231f679d79cc31c0aafe
Changer0001 <yilmmazburak@gmail.com>
2025-04-30 19:15:34 -0700

    Update client.py

--- client/client.py
+++ client/client.py
@@ -1,3 +1,6 @@
+from secure_crypto.rsa_utils import generate_rsa_keypair, rsa_encrypt, rsa_decrypt
+from secure_crypto.aes_utils import aes_encrypt, aes_decrypt
+from secure_crypto.hmac_utils import generate_hmac, verify_hmac
 import socket
 
 def start_client(server_host='localhost', server_port=9999):
